915b9eb5e9414a34f82fd720c971b089adc20208
smsajid <smsajid@users.noreply.github.com>
2024-11-06 06:53:22 +0000

    deploy: 61a61297500bd14d1d1bdc661c7907a1f2d54574

--- core/_static/documentation_options.js
+++ core/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.0 (Draft 2024-10-26)',
+    VERSION: '0.1.0 (Draft 2024-11-6)',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
